1b1cdb47158d675242afd13fc435aaf4f51ab93a
cMartinez13 <camilamartinez8134@gmail.com>
2023-11-29 13:05:36 -0300

    Modelo coodinador y archivo migraciones

--- src/app_reservas/models.py
+++ src/app_reservas/models.py
@@ -8,3 +8,10 @@ class Empleado(models.Model):
     apellido = models.CharField(max_length=255)
     legajo = models.IntegerField()
     activo = models.BooleanField(default=True)
+
+class Coordinador(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    documento = models.IntegerField()
+    fecha_alta = models.DateTimeField()
+    activo = models.BooleanField(default=True) 

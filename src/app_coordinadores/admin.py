e572ec4bf57d92454d6abbc76dc16796b2d5645b
Sebastian Martinez <sebaamartinez54@gmail.com>
2023-11-29 19:37:05 -0300

    Se agrego Coordinador a Admin site.

--- src/app_coordinadores/admin.py
+++ src/app_coordinadores/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Coordinador
 
 # Register your models here.
-admin.site.register(Coordinador)
+
+
+@admin.register(Coordinador)
+class CoordinadorAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido', 'documento', 'fecha_alta', 'activo')
+    search_fields = ('nombre', 'apellido')
+    list_filter = ('activo',)

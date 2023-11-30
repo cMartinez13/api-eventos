40bd69cfcf1ac37a3e7d50121747f3968832d7ce
cMartinez13 <camilamartinez8134@gmail.com>
2023-11-30 12:11:59 -0300

    reserva servicio admin

--- src/app_ReservaServicio/admin.py
+++ src/app_ReservaServicio/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ReservaServicio
 
 # Register your models here.
+@admin.register(ReservaServicio)
+class ReservaServicioAdmin(admin.ModelAdmin):
+    list_display = ('cliente','servicio', 'fecha_reserva', 'fecha_servicio', 'empleado', 'coordinador' )
+    search_fields = ('cliente', 'servicio', 'fecha_reserva')
+    

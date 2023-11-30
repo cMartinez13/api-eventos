7ea2e57e4f3bdd24f8904f3338dfb7ad560a09c4
gabriel <lucasgab2010@gmail.com>
2023-11-30 10:45:07 -0300

    Se registro modelo cliente en admin.py.

--- src/app_clientes/admin.py
+++ src/app_clientes/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from .models import Cliente
 
 # Register your models here.
-admin.site.register(Cliente)
+#admin.site.register(Cliente)
+
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido', 'activo')
+    search_fields = ('nombre', 'apellido')
+    list_filter = ('activo',)

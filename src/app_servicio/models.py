76144779d9db6838f747c23b56036959e7fb1635
gabriel <lucasgab2010@gmail.com>
2023-12-01 19:43:26 -0300

    creaciòn entidad servicio

--- src/app_servicio/models.py
+++ src/app_servicio/models.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 # Create your models here.
 class Servicio(models.Model):
     nombre = models.CharField(max_length=30)
-    descripcion = models.TextField()
+    descripcion = models.TextField(max_length=300)
     precio = models.FloatField()
     activo = models.BooleanField(default=True)
     

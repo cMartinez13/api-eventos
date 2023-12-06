7f65db7260a48299929242f28d5b4905b5b49a1a
Sebastian Martinez <sebaamartinez54@gmail.com>
2023-12-06 04:51:15 -0300

    Se actualizo templates con bootstrap

--- src/app_empleados/views.py
+++ src/app_empleados/views.py
@@ -4,15 +4,20 @@ from django.urls import reverse_lazy
 from .models import Empleado
 
 # Create your views here.
+
+
 class EmpleadoCreateView(generic.CreateView):
     model: Empleado
     fields = '__all__'
     template_name = 'empleados/crear_empleado.html'
+    extra_context = {'titulo': 'Alta de Empleado', 'mensaje_boton': 'CREAR'}
+    success_url = reverse_lazy('empleados:listar')
+
 
 class EmpleadoListView(generic.ListView):
-    queryset = Empleado.objects.filter(activo=True)
+    # queryset = Empleado.objects.filter(activo=True)
+    queryset = Empleado.objects.all()
     model = Empleado
     fields = '__all__'
     context_object_name = 'empleados'
-    template_name= 'empleados/listar_empleado.html'
-
+    template_name = 'empleados/listar_empleado.html'

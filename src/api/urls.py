ba151684bd684de2fc8104e389b42160e0302289
gabriel <lucasgab2010@gmail.com>
2023-12-05 21:56:05 -0300

    Creación endpoint listar empleados.

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,10 +1,13 @@
 
 from django.urls import path
 from . import views
+from .views import ListaEmpleados, DetalleEmpleado
 
 
 app_name = 'api'
 
 urlpatterns = [
+    path('api/empleados/', ListaEmpleados.as_view(), name='lista_empleados'),
+    path('api/empleados/<int:pk>/', DetalleEmpleado.as_view(), name='detalle_empleado'),
 
 ]

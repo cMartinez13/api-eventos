9525d28802bcf763157dc9b9353dc8f76d590cea
cMartinez13 <camilamartinez8134@gmail.com>
2023-12-07 18:09:33 -0300

    Endpoint api servicios y coordinadores

--- src/api/urls.py
+++ src/api/urls.py
@@ -9,5 +9,10 @@ app_name = 'api'
 urlpatterns = [
     path('empleados/', views.ListaEmpleados.as_view(), name='lista_empleados'),
     path('empleados/<int:pk>/', views.DetalleEmpleado.as_view(), name='detalle_empleado'),
+    path('coordinadores/', views.ListaCoordinadores.as_view(), name='lista_coordinadores'),
+    path('coordinadores/<int:pk>/', views.DetalleCoordinadores.as_view(), name='detalle_coordinadores'), 
+    path('servicios/', views.ListaServicios.as_view(), name='lista_servicios'),
+    path('servicios/<int:pk>/', views.DetalleServicios.as_view(), name='detalle_servicios'),
+
 
 ]

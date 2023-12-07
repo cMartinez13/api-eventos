7091011cafd7a608589ee24735e71db270e3afed
Sebastian Martinez <sebaamartinez54@gmail.com>
2023-12-07 17:39:44 -0300

    Api: Se agrego el segundo serializador de Empelados

--- src/api/serializers.py
+++ src/api/serializers.py
@@ -3,6 +3,12 @@ from rest_framework import serializers
 from app_empleados.models import Empleado
 
 
+class EmpleadoSerializerList(serializers.ModelSerializer):
+    class Meta:
+        model = Empleado
+        fields = ['id', 'nombre']
+
+
 class EmpleadoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Empleado
--- src/api/views.py
+++ src/api/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from rest_framework import generics
 from app_empleados.models import Empleado
-from .serializers import EmpleadoSerializer
+from .serializers import EmpleadoSerializer, EmpleadoSerializerList
 
 # Create your views here.
 
 
 class ListaEmpleados(generics.ListAPIView):
     queryset = Empleado.objects.all()
-    serializer_class = EmpleadoSerializer
+    serializer_class = EmpleadoSerializerList
+
 
 class DetalleEmpleado(generics.RetrieveAPIView):
     queryset = Empleado.objects.all()

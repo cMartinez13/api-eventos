666d3f442b4b46bfb2c12be17265932a6d3fb33a
Shugo <metalshugo@gmail.com>
2023-12-11 18:14:43 -0300

    endpoints de la api para clientes

--- src/api/urls.py
+++ src/api/urls.py
@@ -13,6 +13,8 @@ urlpatterns = [
     path('coordinadores/<int:pk>/', views.DetalleCoordinadores.as_view(), name='detalle_coordinadores'), 
     path('servicios/', views.ListaServicios.as_view(), name='lista_servicios'),
     path('servicios/<int:pk>/', views.DetalleServicios.as_view(), name='detalle_servicios'),
+    path('clientes/', views.ListaClientes.as_view(), name='lista_clientes'),
+    path('clientes/<int:pk>/', views.DetalleClientes.as_view(), name='detalle_clientes'),
 
 
 ]

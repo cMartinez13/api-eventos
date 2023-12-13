336dbc2bd7976d6a8ce860aca4f74e29b71a1c0d
Sebastian Martinez <sebaamartinez54@gmail.com>
2023-12-12 21:43:52 -0300

    Fix url reservas servicio

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,20 +1,21 @@
 
 from django.urls import path
 from . import views
-# from .views import ListaEmpleados, DetalleEmpleado
 
 
 app_name = 'api'
 
 urlpatterns = [
     path('empleados/', views.ListaEmpleados.as_view(), name='lista_empleados'),
-    path('empleados/<int:pk>/', views.DetalleEmpleado.as_view(), name='detalle_empleado'),
-    path('coordinadores/', views.ListaCoordinadores.as_view(), name='lista_coordinadores'),
-    path('coordinadores/<int:pk>/', views.DetalleCoordinadores.as_view(), name='detalle_coordinadores'), 
+    path('empleados/<int:pk>/', views.DetalleEmpleado.as_view(),
+         name='detalle_empleado'),
+    path('coordinadores/', views.ListaCoordinadores.as_view(),
+         name='lista_coordinadores'),
+    path('coordinadores/<int:pk>/', views.DetalleCoordinadores.as_view(),
+         name='detalle_coordinadores'),
     path('servicios/', views.ListaServicios.as_view(), name='lista_servicios'),
-    path('servicios/<int:pk>/', views.DetalleServicios.as_view(), name='detalle_servicios'),
-    path('clientes/', views.ListaClientes.as_view(), name='lista_clientes'),
-    path('clientes/<int:pk>/', views.DetalleClientes.as_view(), name='detalle_clientes'),
+    path('servicios/<int:pk>/', views.DetalleServicios.as_view(),
+         name='detalle_servicios'),
 
 
 ]
--- src/app_reservas_servicios/urls.py
+++ src/app_reservas_servicios/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from . import views
 
-
 app_name = 'reserva_servicios'
 
 urlpatterns = [
-    #path('listado/', views.listado_reservas, name='listado_reservas'),
-    path('eliminar/<int:pk>/',
-         views.ReservasDeleteView.as_view(), name='eliminar'),
-    path('nuevo/', views.ReservasCreateView.as_view(), name='crear'),
-    path('modificar/<int:pk>/', views.ResevasUpdateView.as_view(), name='modificar'),
+
+    path('nuevo/', views.ReservasCreateView.as_view(), name='nuevo'),
     path('listar/', views.ReservasListView.as_view(), name='listar'),
+    path('modificar/<int:pk>/', views.ResevasUpdateView.as_view(), name='modificar'),
+    path('eliminar/<int:pk>/', views.ReservasDeleteView.as_view(), name='eliminar'),
+
+    # Falta detalle.
 ]
